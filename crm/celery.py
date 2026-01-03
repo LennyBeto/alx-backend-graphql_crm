d9d0c6c4505444d27a5c6fa4f5ad536ef396b806
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2026-01-03 22:39:32 +0300

    Reformat celery.py for consistency

--- crm/celery.py
+++ crm/celery.py
@@ -1,4 +1,4 @@
-import os
+import os 
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crm.settings")
@@ -13,4 +13,5 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Discover tasks.py in installed apps 
 app.autodiscover_tasks()
+
 
